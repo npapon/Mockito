58ef95a022a852a49178947e490f66ab82ad6fb4
Papoune <npapon@live.fr>
2020-11-29 14:42:05 +0100

    test avec mockito

--- Mockito/src/test/java/service/calculateurServiceTest.java
+++ Mockito/src/test/java/service/calculateurServiceTest.java
@@ -1,27 +1,66 @@
 package service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import bean.Calculateur;
 import bean.Operateur;
 import modeleDonnees.ModeleDeCalcul;
 
+@ExtendWith( MockitoExtension.class )
 class calculateurServiceTest {
 
-    Calculateur        calculateur        = new Calculateur();
-    //
-    CalculateurService calculateurService = new CalculateurServiceImplementation( calculateur );
+    /*
+     * Calculateur calculateur = new Calculateur(); // CalculateurService
+     * calculateurService = new CalculateurServiceImplementation( calculateur );
+     */
+
+    @Mock
+    Calculateur        calculateur;
+    CalculateurService calculateurService;
+
+    @BeforeEach
+    public void init() {
+        calculateurService = new CalculateurServiceImplementation( calculateur );
+    }
+    /*
+     * @Test void testretourneModeleDeCalculAvecSolution() { ModeleDeCalcul
+     * modeleDeCalculSansSolution = ModeleDeCalcul
+     * .creerUnModeleDeCalculAPartirDuneOperationSaisieEnString( "6 + 5" );
+     * 
+     * assertEquals( calculateurService.retourneModeleDeCalculAvecSolution(
+     * modeleDeCalculSansSolution ).getResultat(), new ModeleDeCalcul(
+     * Operateur.ADDITION, 6, 5, 11 ).getResultat() );
+     * 
+     * }
+     */
 
     @Test
-    void testretourneModeleDeCalculAvecSolution() {
-        ModeleDeCalcul modeleDeCalculSansSolution = ModeleDeCalcul
-                .creerUnModeleDeCalculAPartirDuneOperationSaisieEnString( "6 + 5" );
+    public void retourneModeleDeCalculAvecSolution_doitUtiliserLeCalculateur_pourFaireUneAddition() {
+        // ETANT DONNE (GIVEN)
+        // when et then return sont des méthodes de mockito qui permettent de
+        // dire dans un cas précis ce que doit retourner la méthode add de
+        // Calculateur sans l'intelligence derriere
+        when( calculateur.add( 6, 5 ) ).thenReturn( 11 );
+        // QUAND (WHEN)
+        ModeleDeCalcul modeleDeCalculSansSolution = new ModeleDeCalcul( Operateur.ADDITION, 6, 5 );
+        // calculateurService.retourneModeleDeCalculAvecSolution va utiliser
+        // add(6,5) de calculateur du mock defini plus haut
+        int solution = calculateurService.retourneModeleDeCalculAvecSolution( modeleDeCalculSansSolution ).getResultat();
 
-        assertEquals( calculateurService.retourneModeleDeCalculAvecSolution( modeleDeCalculSansSolution ).getResultat(),
-                new ModeleDeCalcul( Operateur.ADDITION, 6, 5, 11 ).getResultat() );
+        // ALORS (THEN)
+
+        // vérifie sert juste à vérifier que le mock est utilisé dans la
+        // simulation de la méthode avec les arguments 6 et 5
+        verify( calculateur ).add( 6, 5 );
+        assertThat( solution ).isEqualTo( 11 );
 
     }
-
 }
